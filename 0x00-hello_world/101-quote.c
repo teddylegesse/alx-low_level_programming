ef7d57c2b951de5748d62d53ebc74326ff5ed1d6
teddylegesse <teddylegesse334@gmail.com>
2022-06-30 09:45:24 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print the size of various tyeps
+ * Return: 1 if exited properiy, non-zero otherwise
+ */
+int main(void)
+{
+write(2, "and that pleace of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
