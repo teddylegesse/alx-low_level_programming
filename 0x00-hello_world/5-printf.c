bd15f2cce70dbe0f18f879139270fc52e3f10548
teddylegesse <teddylegesse334@gmail.com>
2022-06-30 08:29:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 
 /**
  * main - print the size of various tyeps
+ * printf - with proper grammar, but the outcome is a piece of art
  * return: 0  if exited properiy, non-zero otherwise
 */
-int main(void)
+void main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
