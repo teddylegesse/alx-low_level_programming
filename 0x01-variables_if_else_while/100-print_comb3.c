f7d26962b8b3158f08f6f947a654a6f2c228e741
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 06:25:36 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the combination of 2digit, followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+int co;
+int st;
+for (co = 48; co <= 57; co++)
+{
+for (st = 49; st <= 57; st++)
+{
+if (st > co)
+{
+putchar(co);
+putchar(st);
+if (co != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
