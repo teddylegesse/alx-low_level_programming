6ca4a18fc9d0775bdd1e8126b3b2095a272b8b5e
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 02:08:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - assign a random number to the variable n each time it is executed
  * based on the if steatment
