04af068b2d655cdd24403156643dab23d52749df
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 05:13:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the string type of intiger, followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+int st;
+for (st = 48; st <= 57; st++)
+putchar(st);
+putchar(10);
+return (0);
+}
