f3b922794761427b74ea7bd8cb947e8b63a8e1dc
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 02:25:06 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is ", n);
 if (n > 0)
 {
-printf("and is greater than 5");
+printf("%d and is greater than 5");
 }
 if (n == 0)
 {
-printf("and is 0");
+printf("%d and is 0");
 }
 if (n < 0 && n != 0)
 {
-printf("and is less than 6 and not 0");
+printf("%d and is less than 6 and not 0");
 }
 printf("\n");
 return (0);
