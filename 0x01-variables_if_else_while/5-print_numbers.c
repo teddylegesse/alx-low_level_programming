ffcc3d19fc9ef72fcefb42d28b075dd8ba3d47b5
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 04:55:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d\n", n);
+}
+return (0);
+}
