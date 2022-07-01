fa1cc69b0ad79e7a3c644070e3da3a7983eb0eda
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 06:51:52 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints the combination of 3digit, followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+int co;
+int st;
+int th;
+for (co = 48; co <= 57; co++)
+{
+for (st = 49; st <= 57; st++)
+{
+for (th = 50; th <= 57; th++)
+{
+if (st > co && th > st)
+{
+putchar(co);
+putchar(st);
+putchar(th);
+if (co != 56 || st != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar(10);
+return (0);
+}
