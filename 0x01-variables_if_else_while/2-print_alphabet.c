775c51eae5d5cfeeadcfc0b43cb55f8d38c883a8
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 04:05:41 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 always
+ */
+int main(void)
+{
+int ch;
+for (ch + 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
