ceb10f20d343faa914903068672775b1ac5812e3
teddylegesse <teddylegesse334@gmail.com>
2022-07-01 05:39:58 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  main - prints the numbers  in base 16, followed by a new line
+ *  Return: 0 always
+ */
+int main(void)
+{
+int ba;
+char ch;
+for (be 48; be <= 57; be++)
+putchar(be);
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(ch);
+putchar(10);
+return (0);
+}
