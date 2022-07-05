22492a31566ee13b73d146d23b0d23276b61e1c1
teddylegesse <teddylegesse334@gmail.com>
2022-07-05 02:48:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the lowercase letters in the alphabet
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+char j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
