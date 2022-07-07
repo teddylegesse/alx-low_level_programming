273417793e6474ca842699f0cb3c70c5e3d52cde
teddylegesse <teddylegesse334@gmail.com>
2022-07-07 17:49:57 +0300

    0x03debugging commits

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+void positive_or_negative(int i)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
