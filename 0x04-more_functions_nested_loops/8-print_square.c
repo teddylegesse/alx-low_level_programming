7eed7e3d4135406e3184293b98e9e9b286b417ef
teddylegesse <teddylegesse334@gmail.com>
2022-07-09 07:02:07 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - function that prints 10 times the numbers, from 0 to 14,
+ * @size: is parameter
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int x, y;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (x = 0; x < sixe; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
