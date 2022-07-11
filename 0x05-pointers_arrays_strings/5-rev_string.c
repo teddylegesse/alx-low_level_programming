4097046171f276e7a112965ce7ba37fefe4d2dd7
teddylegesse <teddylegesse334@gmail.com>
2022-07-11 04:30:20 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,13 @@ int i, count;
 int length = 0;
 for (i = 0; s[i]; i++)
 {
-end_c++;
+length++;
 }
 count = length;
 start_c = s;
 end_c = s;
+for (i = 0; i < count - 1; i++)
+end_c++;
 for (i = 0; i < count / 2; i++)
 {
 c = *end_c;
