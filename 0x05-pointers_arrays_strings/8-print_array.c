1b5c1ff99a1c5caccea2eebd44302b0b185d0dcd
teddylegesse <teddylegesse334@gmail.com>
2022-07-11 04:12:02 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_array - function that prints n elements of an array of integers
+ * @a: argument
+ * @n: is parameter
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+
+
+
+}
