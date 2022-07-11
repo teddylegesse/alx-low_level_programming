0b653cb1f7187bd23d09737cc22f06a479ca3627
teddylegesse <teddylegesse334@gmail.com>
2022-07-11 11:43:57 +0300

    second comiit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: is string parameter
+ * Return: always 0.
+ */
+int _strlen(char *s)
+{
+int length;
+for (; *s != '\0'; s++)
+{
+length += 1;
+}
+return (length);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - function that prints a string, in reverse,
+ * @s: is parameter
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+int x, y;
+y = 0;
+while (s[y] != '\0')
+y++;
+for ( x = y - 1; x >= 0; x++)
+{
+_putchar (s[x]);
+}
+_putchar('\n');
+}
+
