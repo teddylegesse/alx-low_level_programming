94985803bfd07f1e57e64e6411958c049fa7b6f8
teddylegesse <teddylegesse334@gmail.com>
2022-07-13 10:19:18 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.hi>
-#include <string.h>
 /**
  * reverse_array - function that reverses the content of an array of integers.
  * @a: is parameter 1
@@ -18,5 +17,4 @@ printf(", ");
 }
 printf("%d", a[i]);
 }
-printf("\n");
 }
