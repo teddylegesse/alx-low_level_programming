5e4470a2aa71b020a696622f5cff58bd373bacba
teddylegesse <teddylegesse334@gmail.com>
2022-07-12 22:22:23 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int i;
-int n;
-for (i = 0; i < n; &&src[i] != '\0'; i++)
+int len = 0, i;
+while (src[len])
+	len++;
+for (i = 0; src[i] != '\0'; i++)
+{
 dest[i] = src[i];
-for (; i < n; i++)
-dest[i] = '\0';
+}
 return (dest);
 }
