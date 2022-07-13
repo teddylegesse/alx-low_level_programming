369379acbe299e82ff098939867e34742fb576cf
teddylegesse <teddylegesse334@gmail.com>
2022-07-13 10:03:08 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * cap_string(
- * @*: is parameter
+ * cap_string - function that capitalizes all words of a string.
+ *  @s: is parameter
  * Return: string
  */
 char *cap_string(char *s)
 {
-char special_char[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+char spl[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 int length = 13;
 int a = 0, i;
 while (s[a])
@@ -14,7 +14,7 @@ while (s[a])
 i = 0;
 while (i < length)
 {
-if ((a == 0 || s[a - 1] == special_char[i]) && (s[a] >= 97 && s[a] <= 122))
+if ((a == 0 || s[a - 1] == spl[i]) && (s[a] >= 97 && s[a] <= 122))
 i++;
 }
 a++;
