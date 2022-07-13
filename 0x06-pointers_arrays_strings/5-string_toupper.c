54ee9f7caf07418a034f3dbde138244a9491557a
teddylegesse <teddylegesse334@gmail.com>
2022-07-13 09:55:04 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * string_toupper - function that changes all lowercase letters of a string to uppercase.
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @*: is parameter
  * @s: is parameter
  * Return: Always 0.
