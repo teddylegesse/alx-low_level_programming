5325cef784170fe4a597eff6fc8fc237bd12fe77
teddylegesse <teddylegesse334@gmail.com>
2022-07-13 10:12:16 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 /**
  * cap_string - function that capitalizes all words of a string.
  *  @s: is parameter
@@ -15,6 +17,7 @@ i = 0;
 while (i < length)
 {
 if ((a == 0 || s[a - 1] == spl[i]) && (s[a] >= 97 && s[a] <= 122))
+	s[a] = s[a] - 32;
 i++;
 }
 a++;
