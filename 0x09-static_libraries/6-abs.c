1a21bb56c9615880ce05cb959f519e20499052bc
teddylegesse <teddylegesse334@gmail.com>
2022-07-22 13:54:21 +0300

    delete

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n: argument
+ * Return: valuse
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = -n;
+return (n);
+}
