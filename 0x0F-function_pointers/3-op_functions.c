5c35e325f9c64b8dcd911c8a923d0f557012f631
teddylegesse <teddylegesse334@gmail.com>
2022-08-03 17:44:55 +0300

    nice fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,12 +4,9 @@
 
 /**
  * op_add - Returns the sum of two integer
- * op_sub - Returns the the differance between two integers
- * op_mul - Returns multiplication of two integer
- * op_div - Returns the division of two integers
- * op_mod - Returns mod of two integers
- * @a: is first integer
- * @b: is second integer
+ * @a: first integer
+ * @b: second integer
+ * Return:  Returns the sum of two integer
  */
 int op_add(int a, int b)
 {
@@ -20,6 +17,7 @@ return (a + b);
  * op_sub - Returns the the differance between two integers
  * @a: first integer
  * @b: second integer
+ * Return: Returns the the differance between two integers
  */
 int op_sub(int a, int b)
 {
@@ -30,6 +28,7 @@ int op_sub(int a, int b)
  * op_mul - Returns multiplication of two integer
  * @a: first integer
  * @b: second integer
+ * Return: Returns multiplication of two integer
  */
 int op_mul(int a, int b)
 {
@@ -40,6 +39,7 @@ return (a * b);
  * op_div - Returns the division of two integers
  * @a: first integer
  * @b: second integer
+ * Return: Returns the division of two integers
  */
 int op_div(int a, int b)
 {
@@ -55,6 +55,7 @@ return (a / b);
  * op_mod - Returns mod of two integers
  * @a: first integer
  * @b: second integer
+ * Return: Returns mod of two integers
  */
 int op_mod(int a, int b)
 {
