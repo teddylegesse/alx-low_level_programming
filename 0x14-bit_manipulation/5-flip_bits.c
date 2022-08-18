013ac18c669fbdc381ba29fea2428f0a2b56259c
teddylegesse <teddylegesse334@gmail.com>
2022-08-18 06:29:26 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns the number of bits you would need
+ * to flip to get from one number to anothe
+ * @n: is the number
+ * @m: the number to flip
+ * Return: the fliped number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
+}
