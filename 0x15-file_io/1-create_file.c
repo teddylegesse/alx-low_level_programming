ade46a6edbedc2ffe054c9d286e590c5bd9b10b0
teddylegesse <teddylegesse334@gmail.com>
2022-08-23 11:13:04 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer for the file name
+ * @text_content: pointer strings write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int _open, _write;
+
+	if (filename == NULL)
+		return (-1);
+
+	_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (_open == -1)
+		return (-1);
+	_write = write(_open, text_content, strlen(text_content));
+
+	if (_write == -1)
+		return (-1);
+	close(_open);
+	return (1);
+}
