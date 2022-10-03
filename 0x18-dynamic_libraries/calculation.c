42d850b39327baa62af6d039447a13025e83d83a
teddylegesse <teddylegesse334@gmail.com>
2022-10-03 07:52:42 +0000

    3rd commit

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition of two number
+ * @a: integer argument
+ * @b: integer argument
+ * Return: a+b
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - substruction of two numbers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: a-b
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - multiplication of two numbers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - division of two numbers
+ * @a: integer argument
+ * @b: integer argument
+ * Return: a/b
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - mod of two number
+ * @a: integer argument
+ * @b: integer argument
+ * Return: a%b
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
