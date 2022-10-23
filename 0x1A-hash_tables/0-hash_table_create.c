c5fd27602bec4e58a4d7e20f9ee2a500bf7df9c1
teddylegesse <teddylegesse334@gmail.com>
2022-10-23 05:44:35 +0000

    first commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -  function that creates a hash table.
+ * hash_table_create - Creates a hash table.
  * @size: The size of the array.
- * Return: pointer to the newly created hash table or NULL
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
